bccf2bfab5c671a0ccfc917d206edf93a3a39edd
Edison Lim <93136751+335743100@users.noreply.github.com>
2021-10-29 09:11:04 -0400

    Revert "Base menu&instructions"

--- WordsGame/EndScreen.java
+++ WordsGame/EndScreen.java
@@ -1,28 +1,23 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class InstructionsMenu here.
+ * Write a description of class EndScreen here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class InstructionsMenu extends World
+public class EndScreen extends World
 {
-    
     private GreenfootImage background;
     public static final Color bgColor = new Color(52, 232, 235);
-    public static final Color titleColor = new Color(255, 0, 0);
-    public static Font titleFont = new Font("Courier New", true, false, 100);
-    private String title = "Instructions";
     
-    private Button backButton;
+    private Button returnButton;
     private GreenfootSound clickSound = new GreenfootSound("Menu Click.wav");
-    
     /**
-     * Constructor for objects of class InstructionsMenu.
+     * Constructor for objects of class EndScreen.
      * 
      */
-    public InstructionsMenu()
+    public EndScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(GameWorld.WORLD_WIDTH, GameWorld.WORLD_HEIGHT, 1);
@@ -31,12 +26,12 @@ public class InstructionsMenu extends World
         background.fill();
         setBackground(background);
         
-        backButton = new Button("Back", Color.BLACK, Color.WHITE, Color.RED);
-        addObject(backButton, GameWorld.WORLD_WIDTH / 8, GameWorld.WORLD_HEIGHT * 9 /10);
+        returnButton = new Button("Return", Color.BLACK, Color.WHITE, Color.RED);
+        addObject(returnButton, GameWorld.WORLD_WIDTH / 8, GameWorld.WORLD_HEIGHT * 9 /10);
     }
     
     public void act(){
-        if(Greenfoot.mouseClicked(backButton)){
+        if(Greenfoot.mouseClicked(returnButton)){
             //menuMusic.stop();
             clickSound.play();
             Greenfoot.setWorld(new MainMenu());
