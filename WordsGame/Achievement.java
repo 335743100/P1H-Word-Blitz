6f8acddf66b0de926c9bbcb28bff43314097ef1a
Stewart Chan <stew.chan@gmail.com>
2021-11-09 09:45:19 -0500

    fixes

--- WordsGame/Achievement.java
+++ WordsGame/Achievement.java
@@ -8,16 +8,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Achievement extends Actor
 {
+    private String myName = "too fast";
     private GreenfootImage image;
     public static final int MEDAL_WIDTH = 960 / 6;
     public static final int MEDAL_HEIGHT = 540 / 6;
     private Color nameColor = new Color(255, 255, 0);
     private Font nameFont = new Font("Courier New", true, true, MEDAL_HEIGHT / 6);
     private String name, text;
-    
+
     private boolean popup;
     private int time;
-    
+
     private GreenfootImage bronzeMedal = new GreenfootImage("BronzeMedal.png");
     private GreenfootImage noBronzeMedal = new GreenfootImage("NoBronzeMedal.png");
     private GreenfootImage silverMedal = new GreenfootImage("SilverMedal.png");
@@ -26,14 +27,12 @@ public class Achievement extends Actor
     private GreenfootImage noGoldMedal = new GreenfootImage("NoGoldMedal.png");
     private GreenfootImage diamondMedal = new GreenfootImage("DiamondMedal.png");
     private GreenfootImage noDiamondMedal = new GreenfootImage("NoDiamondMedal.png");
+
     
-    AchievementDescription description;
-    
-    private MouseInfo mouse;
-    
+
     public Achievement(String name, String text, String type, boolean achieved, boolean popup){
         image = new GreenfootImage(MEDAL_WIDTH + 1, MEDAL_HEIGHT + 1);
-        
+
         if(achieved){
             if(type.equals("bronze")){
                 image.drawImage(bronzeMedal, MEDAL_WIDTH / 2 - 34, 0);
@@ -62,10 +61,10 @@ public class Achievement extends Actor
                 image.drawImage(noDiamondMedal, MEDAL_WIDTH / 2 - 34, 0);
             }
         }
-        
+
         this.popup = popup;
         if(popup) time = 180;
-        
+
         this.name = name;
         this.text = text;
         image.setColor(nameColor);
@@ -73,33 +72,44 @@ public class Achievement extends Actor
         image.drawString(name, (image.getWidth() - (int)(name.length() * nameFont.getSize() * 0.58)) / 2, (image.getHeight() * 9 / 10));
         setImage(image);
     }
-    
-    protected void addedToWorld(World AchievementMenu){
-        if(!popup){
-            description = new AchievementDescription(text);
-            getWorld().addObject(description, this.getX(), this.getY());
-        }
-    }
-    
+
+   
+
     /**
      * Act - do whatever the Achievement wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private boolean showDescription;
     public void act() 
     {
-        mouse = Greenfoot.getMouseInfo();
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        if(mouse == null){
+            return;
+        }
         
-        if(popup){
-            if(time == 0) getWorld().removeObject(this);
-            else time--;
+        if(mouse.getActor() == this)
+        {
+            showDescription = true;
         }
-        else{
-            if(Greenfoot.mouseMoved(this)){
-                description.update(true);
-            }
-            if(Greenfoot.mouseMoved(null) && !Greenfoot.mouseMoved(this)){
-                description.update(false);
-            }
+        else if(mouse.getActor() != null)
+        {
+            showDescription = false;   
         }
+
+        if(showDescription)
+        {
+            GreenfootImage image = new GreenfootImage(5,5);
+            setImage(image);
+        }
+        else
+        {
+            setImage(image);
+        }
+        
+        // if(popup){
+            // if(time == 0) getWorld().removeObject(this);
+            // else time--;
+        // }
+
     }
 }
