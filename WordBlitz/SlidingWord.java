4a10a58f8ec4bb2ecf9812313aeb9ec7175e74ef
Jaylen Cheung <93136926+Jaylen05@users.noreply.github.com>
2021-11-10 18:40:38 -0500

    Removed bolded sliding word

---
+++ WordBlitz/SlidingWord.java
@@ -0,0 +1,63 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Slides a word into the WordBox
+ * 
+ * @author Edison Lim, Vaughn Chan, Jaylen Cheung
+ * @version November 9, 2021
+ */
+public class SlidingWord extends Actor
+{
+    // Actor Dimensions
+    public static final int WIDTH = GameWorld.WIDTH;
+    public static final int HEIGHT = GameWorld.HEIGHT;
+    
+    // Image Variables
+    public static final Color WORD_COLOR = new Color(255, 255, 255);
+    public static final Font WORD_FONT = new Font("Courier New", false, false, HEIGHT / 16);
+    
+    // Variables to keep track of the sliding word
+    private String slidingWord = "";
+    private int slidingX = 0;
+    private int slidingY = 0;
+    private int slidingYEnd = 0;
+    
+    /**
+     * Constructor for the sliding word
+     */
+    public SlidingWord() {
+        setImage(new GreenfootImage(WIDTH, HEIGHT));
+    }
+    
+    /**
+     * Act method. Slides a word if there is one in queue
+     */
+    public void act() {
+        if (slidingWord.length() > 0) {
+            if (slidingY < slidingYEnd) {
+                GreenfootImage display = new GreenfootImage(WIDTH, HEIGHT);
+                display.setFont(WORD_FONT);
+                display.setColor(Color.WHITE);
+                display.drawString(slidingWord, slidingX, slidingY);
+                slidingY+=3;
+                setImage(display);
+            } else {
+                slidingWord = "";
+            }
+        }
+    }
+    
+    /**
+     * Set a new word to slide
+     * @param word Word to slide
+     * @param x Starting x value
+     * @param y Starting y value
+     * @param y2 Ending y value
+     */
+    public void setSlidingWord(String word, int x, int y, int y2) {
+        slidingWord = word;
+        slidingX = x;
+        slidingY = y;
+        slidingYEnd = y2;
+    }
+}
